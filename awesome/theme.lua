d10201bcef40d6bed10768d8948a4ccb70911b33
Fabian Furger <furgerfabian@hotmail.com>
2017-08-15 14:00:42 +0200

    Various theme updates

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -10,19 +10,23 @@ local theme = {}
 
 theme.font      = "DejaVu Sans 8"
 
+-- TODO: Remove all the unnecessary/unused stuff
+
 -- base colors
 theme.colors = {}
 theme.colors.white   = "#ffffff"
 theme.colors.grey    = "#aaaaaa"
-theme.colors.light4  = "#D1E9F6"
-theme.colors.light3  = "#A2D4ED"
-theme.colors.light2  = "#74BEE3"
-theme.colors.light1  = "#45A9DA"
+theme.colors.light  = "#D1E9F6"
+-- theme.colors.light4  = "#D1E9F6"
+-- theme.colors.light3  = "#A2D4ED"
+-- theme.colors.light2  = "#74BEE3"
+-- theme.colors.light1  = "#45A9DA"
 theme.colors.arch    = "#1793d1"
-theme.colors.dark1   = "#1276A7"
-theme.colors.dark2   = "#0E587D"
-theme.colors.dark3   = "#093B54"
-theme.colors.dark4   = "#051D2A"
+-- theme.colors.dark1   = "#1276A7"
+-- theme.colors.dark2   = "#0E587D"
+-- theme.colors.dark3   = "#093B54"
+-- theme.colors.dark4   = "#051D2A"
+theme.colors.dark   = "#051D2A"
 theme.colors.black   = "#000000"
 theme.colors.pink    = "#ff0022"
 -- }}}
@@ -33,8 +37,8 @@ theme.fg_normal  = theme.colors.grey
 theme.bg_normal  = theme.colors.black
 theme.fg_focus   = theme.colors.black
 theme.bg_focus   = theme.colors.arch
-theme.fg_urgent  = theme.colors.dark2
-theme.bg_urgent  = theme.colors.light4
+theme.fg_urgent  = theme.colors.dark
+theme.bg_urgent  = theme.colors.light
 theme.bg_systray = theme.bg_normal
 theme.bg_minimize= theme.colors.pink --
 theme.fg_minimize= theme.colors.pink --
@@ -57,19 +61,22 @@ theme.mouse_finder_color = theme.colors.pink
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 
 -- widgets
-theme.cpu_bg      = theme.bg_normal
-theme.cpu_fg      = theme.colors.dark1
-theme.cpu_border  = theme.colors.dark4
-theme.mem_bg      = theme.bg_normal
-theme.mem_fg      = theme.colors.dark1
-theme.mem_border  = theme.colors.dark4
+theme.vol_bg      = theme.bg_urgent
+theme.cpu_bg      = theme.fg_urgent
+theme.cpu_fg_low  = theme.fg_normal
+theme.cpu_fg_high = theme.bg_focus
+theme.cpu_border  = theme.fg_urgent
+theme.mem_bg      = theme._normal
+theme.mem_fg_low  = theme.fg_normal
+theme.mem_fg_high = theme.bg_focus
+theme.mem_border  = theme.fg_urgent
 theme.hdd_bg      = theme.bg_normal
-theme.hdd_fg      = theme.colors.dark1
-theme.hdd_border  = theme.colors.dark4
+theme.hdd_fg_low  = theme.fg_normal
+theme.hdd_fg_high = theme.bg_focus
+theme.hdd_border  = theme.fg_urgent
 theme.hdd_bg      = theme.bg_normal
 theme.batt_bg     = theme.bg_normal
 theme.net_bg      = theme.bg_normal
-theme.vol_bg      = theme.bg_normal
 -- }}}
 
 -- {{{ Borders
@@ -106,7 +113,7 @@ theme.menu_fg_normal = theme.fg_focus
 theme.menu_bg_focus = theme.bg_normal
 theme.menu_fg_focus = theme.fg_normal
 
-theme.menu_border_color = theme.colors.dark4
+theme.menu_border_color = theme.colors.fg_urgent
 theme.menu_border_width = dpi(2)
 -- }}}
 
@@ -148,6 +155,7 @@ theme.widget_hdd                                = themes_path .. "/archdove/icon
 theme.widget_music                              = themes_path .. "/archdove/icons/music.png"
 theme.widget_task                               = themes_path .. "/archdove/icons/task.png"
 theme.widget_mail                               = themes_path .. "/archdove/icons/mail.png"
+theme.widget_caps                               = themes_path .. "/archdove/icons/caps.png"
 theme.arr1                                      = themes_path .. "/archdove/icons/arr1.png"
 theme.arr2                                      = themes_path .. "/archdove/icons/arr2.png"
 theme.arr3                                      = themes_path .. "/archdove/icons/arr3.png"
@@ -184,6 +192,8 @@ theme.ethernet              = themes_path .. "/archdove/icons/ethernet.png"
 theme.config_icon       = themes_path .. "/archdove/icons/config.png"
 theme.explorer_icon       = themes_path .. "/archdove/icons/explorer.png"
 theme.terminal_icon       = themes_path .. "/archdove/icons/terminal.png"
+theme.layout_ch       = themes_path .. "/archdove/icons/ch.png"
+theme.layout_us       = themes_path .. "/archdove/icons/us.png"
 -- }}}
 
 -- {{{ Layout icons
